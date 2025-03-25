e1fe1b653581892a03b20a2cea779d2432655fe0
Adriana <vadriana258@gmail.com>
2025-03-25 01:10:36 +0100

    educator role added and part of settings for img

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,27 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+
 const app = express();
 //connect to database
 
 await connectDB();
 //middlewares
 app.use(cors());
+app.use(clerkMiddleware());
+app.use((req, res, next) => {
+  console.log("Authorization Header:", req.headers["authorization"]);
+  console.log("Auth Object:", req.auth); // Check if Clerk's session info is populated
+  next();
+});
 
 //routes
 app.get("/", (req, res) => res.send("API Working"));
 app.post("/clerk", express.json(), clerkWebhooks);
-
-// port
+app.use("/api/educator", express.json(), educatorRouter);
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,11 +3,13 @@ import { createContext } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
-
+  const { getToken } = useAuth();
+  const { user } = useUser();
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
   const [enrolledCourses, setEnrolledCourses] = useState([]);
@@ -61,6 +63,21 @@ export const AppContextProvider = (props) => {
     fetchAllCourses();
     fetchUserEnrolledCourses();
   }, []);
+  const logToken = async () => {
+    if (user) {
+      const token = await getToken();
+      console.log(token);
+    } else {
+      console.log("User is not logged in.");
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      logToken();
+    }
+  }, [user]);
+
   const value = {
     currency,
     allCourses,
